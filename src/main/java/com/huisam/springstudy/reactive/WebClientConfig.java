d31ae85cd31cb7ad226ef2d05171d0bf767fffe1
huisam <huisam@naver.com>
2020-06-04 22:52:05 +0900

    feat: Add webClient codec - used by objectMapper

--- src/main/java/com/huisam/springstudy/reactive/WebClientConfig.java
+++ src/main/java/com/huisam/springstudy/reactive/WebClientConfig.java
@@ -1,12 +1,16 @@
 package com.huisam.springstudy.reactive;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.TcpClient;
@@ -14,7 +18,10 @@ import reactor.netty.tcp.TcpClient;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebClientConfig {
+
+    final ObjectMapper objectMapper;
 
     @Bean
     public WebClient webClient() {
@@ -27,6 +34,14 @@ public class WebClientConfig {
         ClientHttpConnector connector = new ReactorClientHttpConnector(HttpClient.from(tcpClient));
         return WebClient.builder()
                 .clientConnector(connector)
+                .codecs(clientCodecConfigurer -> {
+                    clientCodecConfigurer.defaultCodecs().jackson2JsonEncoder(
+                            new Jackson2JsonEncoder(objectMapper)
+                    );
+                    clientCodecConfigurer.defaultCodecs().jackson2JsonDecoder(
+                            new Jackson2JsonDecoder(objectMapper)
+                    );
+                })
                 .build();
     }
 }
